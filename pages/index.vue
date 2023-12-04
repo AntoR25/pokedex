502128a038513df40eac3ad8b503ce64d2f76f1f
AntoR25 <antoine.remacle2407@gmail.com>
2023-12-04 10:02:17 +0100

    r4

--- pages/index.vue
+++ pages/index.vue
@@ -26,18 +26,20 @@ pokemons.value = data.value.pokemons;
 </script>
 
 <template>
-  <ul
-    v-if="pokemons"
-    class="grid gap-8 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6"
-  >
-    <li v-for="pokemon in pokemons" :key="pokemon.id">
-      <NuxtLink :to="`/pokemon/${pokemon.slug}`">
-        <NuxtImg :src="pokemon.image.url" :alt="pokemon.nom" />
-        <h2 class="flex flex-wrap justify-start text-3xl font-semibold text-gray-800 pt-2">{{ pokemon.nom }}</h2>
-      </NuxtLink>
-    </li>
-  </ul>
-  <ul v-else>
-    <li>Loading...</li>
-  </ul>
+  <div>
+    <ul
+      v-if="pokemons"
+      class="bg-white bg-opacity-20 p-4 rounded-md grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8"
+    >
+      <li v-for="pokemon in pokemons" :key="pokemon.id" class="flex flex-col items-center p-4 border border-gray-200 rounded-md hover:shadow-lg transition-transform duration-300 transform hover:scale-105">
+        <NuxtLink :to="`/pokemon/${pokemon.slug}`">
+          <NuxtImg :src="pokemon.image.url" :alt="pokemon.nom" class="w-full h-40 object-cover rounded-md mb-4" />
+          <h2 class="text-lg font-semibold text-gray-800">{{ pokemon.nom }}</h2>
+        </NuxtLink>
+      </li>
+    </ul>
+    <ul v-else>
+      <li>Loading...</li>
+    </ul>
+  </div>
 </template>
