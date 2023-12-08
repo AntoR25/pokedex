e6994259ac2a656b19349d773f91c1da2999711b
AntoR25 <antoine.remacle2407@gmail.com>
2023-12-08 12:09:29 +0100

    r8

--- pages/pokemon/[slug].vue
+++ pages/pokemon/[slug].vue
@@ -80,7 +80,12 @@ pokemon.value = data.value.pokemon;
       </div>
       <img class="h-10 w-10 mx-3" :src="pokemon.typePokemon.image.url" />
     </h2>
-    <p class="text-justify text-red-950 pb-10">{{ pokemon.description }}</p>
+    <div class="bg-gradient-to-br from-gray-800 to-gray-700 p-6 md:p-8 lg:p-10 xl:p-12 rounded-lg shadow-xl max-w-2xl mx-auto">
+      <h1 class="text-3xl font-bold text-white mb-4">Description :</h1>
+
+      <p class="text-justify text-white">{{ pokemon.description }}</p>
+</div>
+<footer class="p-10"></footer>
   </div>
   <div v-else>
     <li>Loading...</li>
